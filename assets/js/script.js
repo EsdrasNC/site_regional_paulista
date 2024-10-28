e5e45db78eb1edac813a21d3bcc8704e81dccdab
Esdras Neres Carneiro <esdras.nc@icloud.com>
2024-10-27 23:37:10 -0300

    Botão de menu
    Foi inserido os botões de menu para a responsividade, finalizar ajustes amanhã na empresa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav");
+function menuShow() {
+  let menuToggle = document.querySelector(".mobile-menu");
+  if (menuToggle.classList.contains("open")) {
+    menuToggle.classList.remove("open");
+  } else {
+    menuToggle.classList.add("open");
+  }
+}
 
-hamburger.addEventListener("click", () => nav.classList.toggle("active"));
-  //video inline
-  var video = document.getElementById("meuVideo");
-  
-  // Verificar se o dispositivo é móvel
-  if (window.innerWidth < 768) {
-    video.setAttribute("playsinline", "true");
-    video.setAttribute("muted", "true");
-    video.play();
-  } 
+//video inline
+var video = document.getElementById("meuVideo");
+
+// Verificar se o dispositivo é móvel
+if (window.innerWidth < 768) {
+  video.setAttribute("playsinline", "true");
+  video.setAttribute("muted", "true");
+  video.play();
+}
